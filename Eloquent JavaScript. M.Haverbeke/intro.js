f60c637c51592c6fabc7a65aee46398ee6f9e4bb
AVBanin <banin.a.v@gmail.com>
2021-06-07 13:51:02 +0300

    Добавляет итоговые функции для intro.

--- Eloquent JavaScript. M.Haverbeke/intro.js
+++ Eloquent JavaScript. M.Haverbeke/intro.js
@@ -1,26 +1,3 @@
-//Функция, создающая массив по первому и последнему числу.
-let range = function (first, last) {
-    let array = [];
-    let current = first;
-    for (i = 0; i < last; i++) {
-        array.push(current);
-        current++; 
-    }
-    return array;
-};
-
-//Функция, сворачивающая массив.
-let sum = function(array) {
-    let total = 0;
-    for (i = 0; i < array.length; i++) {
-        total += array[i];
-    }
-    return total;
-};
-
-console.log(sum(range(1, 10)));
-
-//С использованием методов массива и усложнений. 
 //Функция, создающая массив в диапазоне между двумя числами.
 let rangeNew = (first, last) => {
     let array = [];
@@ -40,21 +17,13 @@ let rangeNew = (first, last) => {
     return array;
 };
 
-console.log(rangeNew(1,3));
-
-//Попытки заставить массив свернуться по методу reduce.
-const reducer = (accumulator, currentValue) => accumulator + currentValue;
-
-console.log(rangeNew(1,3).reduce(reducer));
-
-let sumNew = rangeNew(1,3).reduce((accumulator, currentValue) => accumulator + currentValue);
-
-console.log(sumNew);
-
-// Не рабочие попытки написать функцию, сворачивающую массив.
-
-let sumNew1 = (array1) => {
-    array1.reduce(reducer);
+//Функция, сворачивающая массив.
+let sum = function(array) {
+    let total = 0;
+    for (i = 0; i < array.length; i++) {
+        total += array[i];
+    }
+    return total;
 };
 
-console.log(sumNew1(rangeNew(1,3)));
+console.log(sum(range(1, 10)));
