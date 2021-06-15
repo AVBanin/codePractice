fac3c2ba700cb8cf52f7d086e01ac018d7a9e2c7
AVBanin <banin.a.v@gmail.com>
2021-06-15 12:04:17 +0300

    Добавляет третье упражнение.

--- Eloquent JavaScript. M.Haverbeke/Chapter2.Module3.js
+++ Eloquent JavaScript. M.Haverbeke/Chapter2.Module3.js
@@ -2,13 +2,44 @@
 let string = '';
 let size = 8;
 for (let i = 1; i < (size + 1) * size; i++) {
-    if (i !== 0 && i % (size + 1) === 0) {
+    if (i % (size + 1) === 0) {
       string += '\n';
     } else if (i % 2 === 0) {
       string += '#';
     } else {
       string += ' ';
     }
+  }
+
+console.log(string);
+
+console.log('\t');
+
+//Произвольное поле с возможностью задать первый символ, реализация через switch.
+let firstSymbol = '#';
+size = 11;
+string = firstSymbol;
+let oddRowLastSymbol = '';
+if (size % 2 === 0) {
+  oddRowLastSymbol = ' ';
+} else {
+  oddRowLastSymbol = '#';
+}
+
+for (let i = 2; i < size * (size + 1); i++) {
+  switch (true) {
+    case (i % (size + 1) === 0):
+      string += '\n';
+      break;
+    case (string[string.length - 1] === '#' || string[(string.length - 2)] + string[(string.length - 1)] === oddRowLastSymbol + '\n'):
+      string += ' ';
+      break;
+    default:
+      string += '#';
+      break;
+  }
 }
 
 console.log(string);
+
+//Чтобы легче создавать такие алгоритмы, можно представлять поле набором клеточек с порядковыми номерами ячеек и символов переноса строки, равными индексу. В этом случае становится понятно, почему первый алгоритм не подходит для поля с нечётным количеством клеточек: ячейки с нечётным индексом будут располагаться одна под другой.
