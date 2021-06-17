eeaced0754b2d5467551cffd9fb2be3863a1cdee
AVBanin <banin.a.v@gmail.com>
2021-06-17 13:22:14 +0300

    Добавляет задачи третьей главы.

--- Eloquent JavaScript. M.Haverbeke/Chapter3.Module1.js
+++ Eloquent JavaScript. M.Haverbeke/Chapter3.Module1.js
@@ -1,3 +1,14 @@
 /*Минимум
 Функция min, которая принимает два аргумента и возвращает их минимум.*/
+let min = (num1, num2) => {
+  if (num1 > num2) {
+    return num2;
+  } else {
+    return num1;
+  }
+}
 
+//test
+console.log(min(-1, 1));
+console.log(min(101, 1));
+console.log(min(1, 1));
